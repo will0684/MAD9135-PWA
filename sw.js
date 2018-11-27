3539b50b64b079db28b57543a8f052da1847293a
will0684 <jordan.walcaraz@gmail.com>
2018-11-27 16:44:15 -0500

    precaching line added

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
-  
+  workbox.precaching.precacheAndRoute([]);
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
