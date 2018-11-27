7977e71db51ac4c3421c06ee6779caed2d0d9c43
will0684 <jordan.walcaraz@gmail.com>
2018-11-27 16:41:42 -0500

    Removed code

--- sw.js
+++ sw.js
@@ -2,55 +2,7 @@ importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox
 
 if (workbox) {
   console.log(`Yay! Workbox is loaded 🎉`);
+  
 } else {
   console.log(`Boo! Workbox didn't load 😬`);
 }
-
-// workboxSW.router.setDefaultHandler({
-//   handler: workboxSW.strategies.cacheFirst()
-// });
-
-var urlsToCache = [
-  '.',
-  'index.html',
-  'indexOffline.html',
-  'images/online.jpg',
-  'images/offline.jpg'
-];
-
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open('MAIN_CACHE')
-    .then(function(cache) {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-const OFFLINE_FALLBACK_PAGE = '/indexOffline.html';
-
-workbox.routing.registerRoute(
-  new RegExp('/'),
-  async ({event}) => {
-    console.log('index.html route activated')
-    try {
-      return await workbox.strategies.networkOnly().handle({event});
-    } catch (error) {
-      return caches.match(OFFLINE_FALLBACK_PAGE);
-    }
-  }
-);
-
-const OFFLINE_FALLBACK_IMAGE = 'images/offline.jpg';
-
-workbox.routing.registerRoute(
-  new RegExp('/images/'),
-  async ({event}) => {
-    console.log('images route activated')
-    try {
-      return await workbox.strategies.networkOnly().handle({event});
-    } catch (error) {
-      return caches.match(OFFLINE_FALLBACK_IMAGE);
-    }
-  }
-);
